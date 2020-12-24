4b7c84f2700d2bd8eb2c30bd6cae0b812775af36
Indi <indiboteju@Indis-MBP.lan>
2020-12-24 11:57:59 -0800

    updated midsection code

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.css';
 import Header from "./Header";
+import Sidebar from "./Sidebar";
 
 function App() {
   return (
@@ -11,7 +12,11 @@ function App() {
       <Header /> 
 
       {/* App Body */}
+      <div className="app__body" >
         {/* Sidebar */}
+        <Sidebar />
+
+      </div>
         {/* Feed */}
         {/* Widgets */}
 
---
+++ src/Sidebar.js
@@ -0,0 +1,35 @@
+import React from 'react'
+import "./Sidebar.css"
+import { Avatar } from '@material-ui/core';
+
+function Sidebar() {
+    return (
+        <div className='sidebar'>
+            <div className="sidebar__top">
+                <img src="https://images.unsplash.com/photo-1579546929518-9e396f3cc809?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max" alt="" />
+                <Avatar className="sidebar__avatar"/>
+                <h2> Indika Boteju</h2>
+                <h4>indikaboteju01@gmail.com</h4>
+            </div>
+            <div className="sidebar__stats">
+                <div className='sidebar__stat'>
+                    <p> Who viewed you</p>
+                    <p className="sidebar__statNumber"> 2,543</p>
+                </div>
+                <div className='sidebar__stat'>
+                    <p> Views on post</p>
+                    <p className="sidebar__statNumber"> 2,448</p>
+                </div>
+                <div className='sidebar__bottom'>
+                    <p> Recent</p>
+                </div>
+
+            </div>
+        
+        
+        
+        </div>
+    )
+}
+
+export default Sidebar
