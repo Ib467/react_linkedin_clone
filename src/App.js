34914e10c3aeca49726ff8e4ba99c51a73ee74f8
Indi <indiboteju@Indis-MBP.lan>
2020-12-28 08:31:34 -0800

    login page changes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useSelector} from 'react-redux';
+import { auth } from './firebase';
 import { selectUser } from "./features/userSlice"
 import Header from "./Header";
 import Sidebar from "./Sidebar";
@@ -11,7 +12,23 @@ function App() {
   //pull user from datastore
   const user = useSelector(selectUser)
 
-
+  useEffect(() => {
+    auth.onAutStateChanged(userAuth => {
+      if (userAuth){
+        //user is logged in
+        dispatch(
+          login({
+            email: userAuth.email,
+            uid: userAuth.uid,
+            displayName: userAuth.displayName,
+            photoUrl: userAuth.photoUrl,
+        }))
+      } else{
+        //user is logged out
+        dispatch(logout());
+      }
+    })
+  }, [])
 
   return (
     <div className="app">
