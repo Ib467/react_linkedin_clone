eccf1e1b3c9dd40601166b43383887a5e037961d
Indi <indiboteju@Indis-MBP.lan>
2021-01-01 22:01:30 -0800

    login flow

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import React from 'react';
-import { useSelector} from 'react-redux';
-import { selectUser } from "./features/userSlice"
+import {useEffect} from 'react';
+import { useSelector, useDispatch} from 'react-redux';
+import { login, logout, selectUser } from "./features/userSlice"
 import Header from "./Header";
+import { auth } from './firebase'
 import Sidebar from "./Sidebar";
 import Feed from './Feed'
 import Login from './Login'
@@ -10,8 +12,25 @@ function App() {
 
   //pull user from datastore
   const user = useSelector(selectUser)
+  const dispatch = useDispatch() ;
 
-
+  useEffect(() => {
+    auth.onAuthStateChanged((userAuth) => {
+      if(userAuth){
+        //user is logged in
+        dispatch(
+          login({
+            email: userAuth.email,
+            uid: userAuth.uid,
+            displayName: userAuth.displayName,
+            profileUrl: userAuth.photoUrl,
+          })
+        )
+      }else{
+        dispatch(logout());
+      }
+    })
+  }, []);
 
   return (
     <div className="app">
